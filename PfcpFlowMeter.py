90243bac85b251df5db4c15573362fd49343f680
MichalTajak <115180191+MichalTajak@users.noreply.github.com>
2025-04-16 13:02:46 +0200

    Update PfcpFlowMeter.py

--- PfcpFlowMeter.py
+++ PfcpFlowMeter.py
@@ -5,7 +5,7 @@ import numpy as np
 import tables as tb
 import argparse
 
-# Mapowanie typów wiadomości PFCP na czytelne nazwy
+# Mapping PFCP message types to readable names
 pfcp_msg_type_map = {
     1: "heartbeat_request",
     2: "heartbeat_response",
@@ -41,7 +41,7 @@ class PcapCsvConverter:
         self.interval = interval
         self.temp_csv_file = os.path.join(output_directory, 'temp_pcap.csv')
 
-        # Wyodrębnij nazwę pliku bez rozszerzenia i dodaj rozszerzenie .csv z interwałem
+        # Extract the file name without extension and add .csv with interval
         file_name = os.path.basename(pcap_file)
         file_base_name = os.path.splitext(file_name)[0]
         self.final_csv_file = os.path.join(output_directory, f'{file_base_name}_{interval}.csv')
@@ -50,22 +50,22 @@ class PcapCsvConverter:
     def convert_pcap_to_csv(self):
         command_csv = [
             'tshark',
-            '-r', self.pcap_file,  # Plik wejściowy .pcap
-            '-Y', 'pfcp',  # Filtr przechwytywania dla PFCP
+            '-r', self.pcap_file,
+            '-Y', 'pfcp',  
             '-T', 'fields',
             '-E', 'separator=,',
             '-E', 'quote=d',
-            '-e', 'frame.time_relative',  # Czas
-            '-e', 'ip.src',  # Adres źródłowy
-            '-e', 'ip.dst',  # Adres docelowy
-            '-e', 'pfcp.msg_type',  # Typ wiadomości PFCP (jako liczba)
-            '-e', 'frame.len',  # Długość ramki
-            '-E', 'header=y'  # Nagłówek CSV
+            '-e', 'frame.time_relative',  
+            '-e', 'ip.src',  
+            '-e', 'ip.dst',  
+            '-e', 'pfcp.msg_type',  # PFCP message type (as number)
+            '-e', 'frame.len',  # Frame length
+            '-E', 'header=y'  # CSV header
         ]
 
         print(f"Converting {self.pcap_file} to {self.temp_csv_file}")
         with open(self.temp_csv_file, 'w') as file:
-            subprocess.run(command_csv, stdout=file)  # Zapis do pliku CSV
+            subprocess.run(command_csv, stdout=file)  # Save to CSV file
         print(f"Conversion completed: {self.temp_csv_file}")
 
     def add_message_type_names(self):
@@ -73,14 +73,14 @@ class PcapCsvConverter:
             lines = file.readlines()
 
         with open(self.final_csv_file, 'w') as file:
-            # Dodaj nagłówki kolumn
+           # Add column headers
             file.write('frame.time,ip.src,ip.dst,pfcp.msg_type,frame.len,info\n')
-            for line in lines[1:]:  # Pomijaj nagłówek
+            for line in lines[1:]:  # Skip header
                 parts = line.strip().split(',')
                 text = parts[3].replace('"', '')
                 try:
                     msg_type_name = pfcp_msg_type_map.get(int(text), 'unknown')  
-                    # Dodaj nową kolumnę 'info' z nazwą typu wiadomości
+                    # Add new column 'info' with message type name
                     file.write(f'{",".join(parts)},{msg_type_name}\n')
                 except:
                     pass
@@ -88,58 +88,52 @@ class PcapCsvConverter:
     def process_csv(self):
         df = pd.read_csv(self.final_csv_file)
 
-        # Sprawdź dane, aby upewnić się, że są poprawne
-        #print(df.head())
-
         min_time = df['frame.time'].min()
         max_time = df['frame.time'].max()
 
-        # Utwórz etykiety przedziałów czasowych
+        # Create time interval labels
         bins = np.arange(min_time, max_time + self.interval, self.interval)
         labels = [f"part_{i + 1}" for i in range(len(bins) - 1)]
 
-        # Przydziel każdemu wierszowi odpowiedni przedział czasowy
         df['part'] = pd.cut(df['frame.time'], bins=bins, labels=labels, include_lowest=True)
 
-        # Grupowanie i liczenie wartości 'info'
+        # Group and count 'info' values
         df_grouped = df.groupby(['part', 'info']).size().reset_index(name='count')
 
-        # Tworzenie tabeli przestawnej z wszystkimi wartościami ze słownika jako kolumny
+        # Create pivot table with all values from the dictionary as columns
         df_final = df_grouped.pivot_table(index='part', columns='info', values='count', fill_value=0)
         for msg_type_name in pfcp_msg_type_map.values():
             if msg_type_name not in df_final.columns:
                 df_final[msg_type_name] = 0
 
-        # Resetowanie indeksu i usuwanie kolumny 'part'
+        # Reset index and remove 'part' column
         df_final.reset_index(inplace=True)
         df_final.drop(columns=['part'], inplace=True)
         if 'Label' in df_final.columns:
-            # Zapis do pliku CSV
             df_final.to_csv(self.final_csv_file, index=False)
         else:
             df_final = self.manual_create_label(df_final)
             df_final.to_csv(self.final_csv_file, index=False)
         
         '''
-        # Zapis wdo pliku HDF5
+        # Save to HDF5 file
         with tb.open_file(self.final_h5_file, mode='w') as h5file:
             h5file.create_table('/', 'pfcp_data', obj=df_final.to_records(index=False))
-
         '''
 
     def manual_create_label(self, df):
         names = ['heartbeat', 'session_deletion', 'session_modification', 'session_establishment']
-        # Dodanie nowych kolumn Label_val i Label do DataFrame
-        df['Label_val'] = 4  # Domyślna wartość to 4
-        df['Label'] = 'mix_att'  # Domyślna wartość to 'mix_att'
+        # Add new columns Label_val and Label to DataFrame
+        df['Label_val'] = 4  # Default value is 4
+        df['Label'] = 'mix_att'  # Default label is 'mix_att'
     
         for index, row in df.iterrows():
             row_to_sum = row.drop(labels=['Label_val','Label'])
-            total_sum = row_to_sum.sum()  # Suma wszystkich kolumn
-            # Sprawdzanie sum wybranych kolumn
+            total_sum = row_to_sum.sum()  # Sum of all columns
+            # Check selected columns
             for i, name in enumerate(names):
-                selected_sum = row[f'{name}_request'] + row[f'{name}_request']  # Suma wybranych kolumn
-                # Sprawdzenie, czy selected_sum stanowi co najmniej 70% total_sum
+                selected_sum = row[f'{name}_request'] + row[f'{name}_request']  # Sum of selected columns
+                # Check if selected_sum is at least 70% of total_sum
                 if i == 0:
                     if selected_sum >= 0.8 * total_sum:
                         df.at[index, 'Label_val'] = i
@@ -159,30 +153,28 @@ class PcapCsvConverter:
         return df
 
     def run(self):
-        # Upewnienie się, że folder wyjściowy istnieje
         if not os.path.exists(self.output_directory):
             os.makedirs(self.output_directory)
 
-        # Konwersja PCAP na CSV
+        # Convert PCAP to CSV
         self.convert_pcap_to_csv()
 
-        # Dodanie nazw typów wiadomości
+        # Add readable message type names
         self.add_message_type_names()
 
-        # Przetwarzanie i podział danych na przedziały czasowe
+        # Process and split data into time intervals
         self.process_csv()
 
-        # Usunięcie tymczasowego pliku CSV
+        # Remove temporary CSV file
         os.remove(self.temp_csv_file)
         print(f"Temporary file {self.temp_csv_file} removed")
 
 
 def process_all_pcaps_in_directory(data_directory, output_directory, interval):
-    # Upewnienie się, że folder wyjściowy istnieje
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
 
-    # Iteracja przez wszystkie pliki w podanym folderze
+    # Iterate through all files in the given folder
     processed_files_count = 0
     for filename in os.listdir(data_directory):
         if filename.endswith('.pcap'):
